835cdc024444c5a21ef0d0a51699571f1ead1566
varsha <vh+github@mvarsha.com>
2017-05-29 18:07:46 +0530

    Initial commit

---
+++ MagicalTask1/MagicalTask1/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  MagicalTask1
+//
+//  Created by Varsha on 29/05/17.
+//  Copyright © 2017 Varsha. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
