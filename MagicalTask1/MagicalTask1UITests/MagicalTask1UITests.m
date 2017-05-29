a88d2fa6f114a6021bf1a9d8a6ca2e6f78883611
varsha <vh+github@mvarsha.com>
2017-05-29 23:36:18 +0530

    ui tests

--- MagicalTask1/MagicalTask1UITests/MagicalTask1UITests.m
+++ MagicalTask1/MagicalTask1UITests/MagicalTask1UITests.m
@@ -35,6 +35,23 @@
 - (void)testExample {
     // Use recording to get started writing UI tests.
     // Use XCTAssert and related functions to verify your tests produce the correct results.
+    [XCUIDevice sharedDevice].orientation = UIDeviceOrientationPortrait;
+    
+    XCUIApplication *app = [[XCUIApplication alloc] init];
+    XCUIElementQuery *tablesQuery = app.tables;
+    XCUIElement *cell = [[tablesQuery childrenMatchingType:XCUIElementTypeCell] elementBoundByIndex:0];
+    [cell.buttons[@"Comments 123"] tap];
+    
+    XCUIElement *okButton = app.alerts[@"Alert"].collectionViews.buttons[@"OK"];
+    [okButton tap];
+    [cell.buttons[@"Support 456"] tap];
+    [okButton tap];
+    [app.buttons[@"LeftTop"] tap];
+    [okButton tap];
+    [app.buttons[@"RightTop"] tap];
+    [[[[[[app.otherElements containingType:XCUIElementTypeButton identifier:@"LeftTop"] childrenMatchingType:XCUIElementTypeOther] elementBoundByIndex:1] childrenMatchingType:XCUIElementTypeOther] elementBoundByIndex:1].buttons[@"Button"] tap];
+    [app.alerts[@"Circle Menu Action"].collectionViews.buttons[@"Ok"] tap];
+    [[tablesQuery.cells containingType:XCUIElementTypeButton identifier:@"Comments 456"].staticTexts[@"How much does a corrupt official earn in India? #Corruption"] tap];
 }
 
 @end
